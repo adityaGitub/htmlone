1272cdadf02731af96e14f69a737462a1c091b42
adityaGitub <122309134+adityaGitub@users.noreply.github.com>
2023-07-18 22:42:23 +0530

    first commit

---
+++ firstc.c/hi.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int  main()
+{
+    int var=3;
+    int num=2;
+    int dumb=4;
+    if (var==num)
+    {
+        printf("ok");
+    }
+    else if (var==dumb)
+    {
+        printf("not ok");
+    }
+    printf("haha");
+    return 1;
+}
+    
+    
+    
